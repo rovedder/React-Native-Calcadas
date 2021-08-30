5e02f1dd4f87cdf4a5403aa86c2481b24c07dc26
Arthur Rovedder <31117536+rovedder@users.noreply.github.com>
2021-08-30 12:04:30 -0300

    api-conector

--- src/pages/VisualizarAvaliacoes.js
+++ src/pages/VisualizarAvaliacoes.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios'
 
 export default function VisualizarAvaliaces(){
+    const [review, setReview] = useState([])
+
+    useEffect(() => {
+        axios.get('http://192.168.100.10:8080/review/reviewList?email=avrovedder@inf.ufsm.br')
+        .then(function (response) {
+            // handle success
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
+    }, [])
+
     return(
         <View> 
             <Text>VisualizarAvaliaces</Text>
